4ef4f0fa423c1bb14bcf4dc7c5e0a58def082340
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2025-04-05 14:42:07 -0300

    Exceção para quando não existe uma matricula cadastrada.

---
+++ src/main/java/com/example/demo/exception/NotRegisterMatriculaUser.java
@@ -0,0 +1,8 @@
+package com.example.demo.exception;
+
+public class NotRegisterMatriculaUser extends RuntimeException{
+        public NotRegisterMatriculaUser(String message) { 
+                super(message);
+        }
+
+}
