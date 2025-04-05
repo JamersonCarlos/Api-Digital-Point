26fbd44cd2a9ddf57c3866f2f73b37d1184c1815
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2025-04-05 14:47:15 -0300

    exceção necessária

---
+++ src/main/java/com/example/demo/exception/NotExistRelationalAndUserException.java
@@ -0,0 +1,8 @@
+package com.example.demo.exception;
+
+public class NotExistRelationalAndUserException extends RuntimeException {
+        public NotExistRelationalAndUserException(String message) { 
+                super(message);
+        }
+
+}
