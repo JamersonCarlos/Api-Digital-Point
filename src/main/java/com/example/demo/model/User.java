a68241e60ba7664499a55bccad6bdf663fac0c1c
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2025-04-05 14:44:54 -0300

    Alterando o model para usar a chave primária como matricula.

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -7,30 +7,40 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="users")
+@Table(name="user")
 public class User implements UserDetails {
 
-    
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private String id; 
-
-    private String name; 
     private String login; 
+
+    private String name;
     private String password; 
 
 
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    @OneToOne
+    @JoinColumn(name = "login", referencedColumnName = "matricula", insertable = false, updatable = false)
+    private UserWorkInfo userWorkInfo;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<HistoricoPontos> historicoPontos;
+
+    public User() {
+        // Pode deixar vazio ou inicializar algum valor padrão
+    }
     
     public User(String name, String login, String password, Role role) {
         this.name = name;
@@ -82,13 +92,6 @@ public class User implements UserDetails {
         this.login = login;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
     
 
     
