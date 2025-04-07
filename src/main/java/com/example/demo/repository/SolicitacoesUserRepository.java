8193a1a84fa2492cdb40ea332685150967efbce2
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2025-04-07 13:16:50 -0300

    Create SolicitacoesUserRepository.java

---
+++ src/main/java/com/example/demo/repository/SolicitacoesUserRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.SolicitacoesUser;
+
+public interface SolicitacoesUserRepository extends JpaRepository<SolicitacoesUser, Integer>{
+        List<SolicitacoesUser> findByUser_Login(String matricula);
+}
