49beb8e85ff61e48a2b17a94a78effd98b51b143
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2025-04-10 09:13:02 -0300

    Criando endpoint para consultar as solicitações
    all - Capturar todas as solicitações
    ponto/{id} - para pegar as solicitações de um determinado ponto.

--- src/main/java/com/example/demo/controller/SolicitacaoController.java
+++ src/main/java/com/example/demo/controller/SolicitacaoController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,27 +36,20 @@ public class SolicitacaoController {
                         token = token.substring(7);
                     }
                 String subject = tokenService.validateToken(token);
-                return ResponseEntity.ok().body(solicitacoesService.abrirSolicitacao(solicitacao, subject));
-        }
-
-        @GetMapping("/all")
-        public ResponseEntity<List<SolicitacaoVO>> getAllSolicitacao(@RequestHeader("Authorization") String token) { 
-                if (token.startsWith("Bearer ")) {
-                        token = token.substring(7);
-                    }
-                String subject = tokenService.validateToken(token);
-                return ResponseEntity.ok().body(solicitacoesService.getSolicitacoes(subject));
-        }
-
-        @GetMapping("/{id}")
-        public ResponseEntity<SolicitacoesUser> getOneLicitacao(@PathVariable int id) { 
-                
-                Optional<SolicitacoesUser> response = solicitacoesService.getOneSolicitacao(id);
-                if (response.isPresent()) {
-                        return ResponseEntity.ok().body(response.get());
-                } else {
-                        return ResponseEntity.notFound().build();
+                SolicitacaoVO novaSolicitacao = solicitacoesService.abrirSolicitacao(solicitacao, subject);
+                if(novaSolicitacao != null) { 
+                        return ResponseEntity.ok().body(novaSolicitacao);
+                } else { 
+                        return ResponseEntity.notFound().build(); 
                 }
         }
+
+        @GetMapping("/ponto/{id}")
+        public ResponseEntity<List<SolicitacaoVO>> getAllSolicitacoesReferencePonto(@RequestHeader("Authorization") String token, @PathVariable int id) { 
+                return ResponseEntity.ok().body(solicitacoesService.getSolicitacoes(id));
+        }
+
+
+     
         
 }
