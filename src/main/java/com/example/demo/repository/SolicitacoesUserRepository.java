fe8f67d3d3b0241f54ecd3900a2e52b7bd055856
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2025-04-10 09:15:28 -0300

    Update SolicitacoesUserRepository.java

--- src/main/java/com/example/demo/repository/SolicitacoesUserRepository.java
+++ src/main/java/com/example/demo/repository/SolicitacoesUserRepository.java
@@ -7,4 +7,5 @@ import com.example.demo.model.SolicitacoesUser;
 
 public interface SolicitacoesUserRepository extends JpaRepository<SolicitacoesUser, Integer>{
         List<SolicitacoesUser> findByUser_Login(String matricula);
+        List<SolicitacoesUser> findAllByHistoricoPontos_Id(Integer idPonto);
 }
