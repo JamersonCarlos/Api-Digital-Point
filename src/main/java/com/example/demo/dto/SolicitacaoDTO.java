e9646ef8c7cd8ac0d40ca4842924823342e1fbe4
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2025-04-10 09:14:14 -0300

    incluindo id_ponto no objeto de transferência.

--- src/main/java/com/example/demo/dto/SolicitacaoDTO.java
+++ src/main/java/com/example/demo/dto/SolicitacaoDTO.java
@@ -2,6 +2,6 @@ package com.example.demo.dto;
 
 import java.time.LocalDateTime;
 
-public record SolicitacaoDTO(boolean anulacao_ponto, boolean alterar_horario_entrada, boolean alterar_horario_saida, LocalDateTime novo_horario_entrada, LocalDateTime novo_horario_saida) {
+public record SolicitacaoDTO(int id_ponto, boolean anulacao_ponto, boolean alterar_horario_entrada, boolean alterar_horario_saida, LocalDateTime novo_horario_entrada, LocalDateTime novo_horario_saida) {
 
 }
