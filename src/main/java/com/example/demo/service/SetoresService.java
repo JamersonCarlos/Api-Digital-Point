937857db168c01f0887a31d976d1ac202e855b12
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2025-04-27 18:18:39 -0300

    métodos para buscar informações de um setor e para setar o chefe do setor

--- src/main/java/com/example/demo/service/SetoresService.java
+++ src/main/java/com/example/demo/service/SetoresService.java
@@ -3,13 +3,17 @@ package com.example.demo.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.model.Departamento;
+import com.example.demo.model.Funcionario;
 import com.example.demo.model.SetorDepartamento;
 import com.example.demo.repository.DepartamentoRepository;
+import com.example.demo.repository.FuncionarioRepository;
 import com.example.demo.repository.SetorDepartamentoRepository;
+
 
 @Service
 public class SetoresService {
@@ -19,6 +23,12 @@ public class SetoresService {
 
         @Autowired
         DepartamentoRepository departamentoRepository; 
+
+        @Autowired 
+        FuncionarioRepository funcionarioRepository; 
+
+        @Autowired
+        private ModelMapper modelMapper = new ModelMapper(); 
 
         public List<SetorDepartamento> getAllSetores(int idDepartamento) { 
                 return setorDepartamentoRepository.findByDepartamentoId(idDepartamento);
@@ -34,6 +44,27 @@ public class SetoresService {
 
         public void deleteSetor(int idSetor) { 
                 setorDepartamentoRepository.deleteById(idSetor);
+        }
+
+        public Optional<SetorDepartamento> getSetor(int idSetor) { 
+                return setorDepartamentoRepository.findById(idSetor);
+        }
+
+        public Funcionario setChefeSetor(int idsetor, String matricula) { 
+                Optional<SetorDepartamento> optional = setorDepartamentoRepository.findById(idsetor);
+                if (optional.isPresent()) {
+                        SetorDepartamento setorDepartamento = optional.get();
+                        Funcionario chefeSet = funcionarioRepository.findByMatricula(matricula); 
+                         if(chefeSet != null) {     
+                                chefeSet.setSetorDepartamento(setorDepartamento);
+                                setorDepartamento.setChefe(chefeSet);
+                                setorDepartamentoRepository.save(setorDepartamento); 
+                                return chefeSet; 
+                        }
+                        return null; 
+                } else {
+                        return null; 
+                }
         }
 
 }
