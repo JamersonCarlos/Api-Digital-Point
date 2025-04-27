67c86f27652cfd4de25940c9b7561fc8b64eaab3
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2025-04-27 18:18:16 -0300

    método para salvar um novo funcionário

--- src/main/java/com/example/demo/service/FuncionarioService.java
+++ src/main/java/com/example/demo/service/FuncionarioService.java
@@ -1,10 +1,15 @@
 package com.example.demo.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.exception.MatriculaJaExisteException;
 import com.example.demo.model.Funcionario;
+import com.example.demo.model.SetorDepartamento;
 import com.example.demo.repository.FuncionarioRepository;
+import com.example.demo.repository.SetorDepartamentoRepository;
 
 
 @Service
@@ -12,9 +17,23 @@ public class FuncionarioService {
 
         @Autowired
         private FuncionarioRepository funcionarioRepository; 
+        
+        @Autowired
+        private SetorDepartamentoRepository setorDepartamentoRepository; 
 
         public Funcionario getDetails(String matricula) { 
                 return funcionarioRepository.findByMatricula(matricula);
+        }
+        
+        public void salvarFuncionario(int idSetor, Funcionario funcionario) { 
+                Optional<SetorDepartamento> setorFuncionario = setorDepartamentoRepository.findById(idSetor);
+
+                if (funcionarioRepository.existsById(funcionario.getMatricula())) {
+                        throw new MatriculaJaExisteException("Funcionário com matrícula " + funcionario.getMatricula() + " já existe.");
+                }
+
+                funcionario.setSetorDepartamento(setorFuncionario.get());
+                funcionarioRepository.save(funcionario);
         }
 
 }
